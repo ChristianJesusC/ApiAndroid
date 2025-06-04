ff74a85bbceb2ecb38edad5caae4de059f854c32
ChristianJesusC <jesuschiu505@gmail.com>
2025-06-03 21:00:37 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import usuarioRoutes from "./usuario/infrastructure/router/usuarioRoutes";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/usuarios", usuarioRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.clear();
