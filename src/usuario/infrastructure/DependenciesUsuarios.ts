438aeacad002b7cf0d1ddf3bb8e946dbc8298cc5
Christian Jesús Chiu Valdivieso <140458869+ChristianJesusC@users.noreply.github.com>
2025-09-25 03:01:59 -0600

    Feature/obtener usuarios (#2)
    * flujo basico de obtencion de todos los usuarios registrados
    
    * Agrega lógica para obtener usuarios
    
    * Agrega lógica para obtener usuarios
    
    ---------
    
    Co-authored-by: Poltergeis <julio.cruzazul@hotmail.com>

--- src/usuario/infrastructure/DependenciesUsuarios.ts
+++ src/usuario/infrastructure/DependenciesUsuarios.ts
@@ -1,26 +1,35 @@
-import { UsuarioRepositoryImpl } from './repository/UsuarioRepositoryImpl'
-import { RegistrarUsuarioCasoUso } from '../application/metodosUsuarios/registrarUsuarioCasoUso'
-import { LoginUsuarioCasoUso } from '../application/metodosUsuarios/loginUsuarioCasoUso'
+import { UsuarioRepositoryImpl } from "./repository/UsuarioRepositoryImpl";
+import { RegistrarUsuarioCasoUso } from "../application/metodosUsuarios/registrarUsuarioCasoUso";
+import { LoginUsuarioCasoUso } from "../application/metodosUsuarios/loginUsuarioCasoUso";
+import { ObtenerUsuariosCasoUso } from "../application/metodosUsuarios/ObtenerUsuariosCasoUso";
 
-import { RegistrarUsuarioController } from './controller/RegistrarUsuarioController'
-import { LoginUsuarioController } from './controller/LoginUsuarioController'
+import { RegistrarUsuarioController } from "./controller/RegistrarUsuarioController";
+import { LoginUsuarioController } from "./controller/LoginUsuarioController";
+import { ObtenerUsuariosController } from "./controller/ObtenerUsuariosController";
 
 // Repositorio (implementación concreta)
-const usuarioRepository = new UsuarioRepositoryImpl()
+const usuarioRepository = new UsuarioRepositoryImpl();
 
 // Casos de uso (inyección del repositorio)
-const registrarUsuarioCasoUso = new RegistrarUsuarioCasoUso(usuarioRepository)
-const loginUsuarioCasoUso = new LoginUsuarioCasoUso(usuarioRepository)
+const registrarUsuarioCasoUso = new RegistrarUsuarioCasoUso(usuarioRepository);
+const loginUsuarioCasoUso = new LoginUsuarioCasoUso(usuarioRepository);
+const obtenerUsuariosCasoUso = new ObtenerUsuariosCasoUso(usuarioRepository);
 
 // Controladores (inyección de casos de uso)
-const registrarUsuarioController = new RegistrarUsuarioController(registrarUsuarioCasoUso)
-const loginUsuarioController = new LoginUsuarioController(loginUsuarioCasoUso)
+const registrarUsuarioController = new RegistrarUsuarioController(
+  registrarUsuarioCasoUso
+);
+const loginUsuarioController = new LoginUsuarioController(loginUsuarioCasoUso);
+const obtenerUsuariosController = new ObtenerUsuariosController(
+  obtenerUsuariosCasoUso
+);
 
 // Exportamos todo para usarlo en rutas u otros lugares
 export {
+  obtenerUsuariosController,
   registrarUsuarioController,
   loginUsuarioController,
   registrarUsuarioCasoUso,
   loginUsuarioCasoUso,
-  usuarioRepository
-}
+  usuarioRepository,
+};
