29f08abb371ede42cbf0cf4a6f7b654a9eb85371
Christian Jesús Chiu Valdivieso <140458869+ChristianJesusC@users.noreply.github.com>
2025-10-02 06:49:15 -0600

    implementacion sin seguridad (pruebas) (#6)

--- src/videojuego/infrastructure/router/juegoRoutes.ts
+++ src/videojuego/infrastructure/router/juegoRoutes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { juegoController } from "../DependenciesJuegos";
 import { upload } from "../../../middlewares/upload";
-import { verificarToken } from "../../../middlewares/authMiddleware";
 
 const router = Router();
 
-router.post("/", verificarToken, upload.single("logo"), juegoController.crearJuego);
+router.post("/", upload.single("logo"), juegoController.crearJuego);
 router.get("/", juegoController.obtenerJuegos); // Esta puede quedarse sin auth para que la app pueda ver los juegos
-router.put("/:id", verificarToken, upload.single("logo"), juegoController.actualizarJuego);
-router.delete("/:id", verificarToken, juegoController.eliminarJuego);
+router.put("/:id", upload.single("logo"), juegoController.actualizarJuego);
+router.delete("/:id", juegoController.eliminarJuego);
 
 export default router;
